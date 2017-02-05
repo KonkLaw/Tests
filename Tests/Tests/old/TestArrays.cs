df525f5ea55d961bbdc6c3f06547658e4bc323a8
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2017-02-05 23:57:05 +0300

    added: old test files

---
+++ Tests/Tests/old/TestArrays.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SimdTest
+{
+	public class TestArrays
+	{
+        private int count;
+        float[] a;
+        float[] b; 
+        float[] res1;
+        float[] res2;
+
+        public TestArrays()
+        {
+            count = 100000000;
+            a = RandomHelper.GetNumbers(count);
+            b = RandomHelper.GetNumbers(count);
+            res1 = new float[count];
+            res2 = new float[count];
+        }
+
+        [Benchmark]
+        public float[] Sum_slow()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                res1[i] = a[i] + b[i];
+            }
+            return res1;
+        }
+
+        [Benchmark]
+        public float[] Sum_fast()
+        {
+            // This caching is REALLY important.
+            float[] a_ = a;
+            float[] b_ = b;
+            float[] res2_ = res2;
+
+            for (int i = 0; i < a_.Length; i++)
+            {
+                res2_[i] = a_[i] + b_[i];
+            }
+            return res2_;
+        }
+    }
+}
---
+++ Tests/Tests/old/TestFloat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace SimdTest
+{
+    static class TestFloat
+    { 
+        internal static void Run()
+        {
+            //const int vectorsCount = 10000000;
+            //int numbersOnVectorCount = Vector<float>.Count;
+            //float[] a = RandomHelper.GetNumbers(numbersOnVectorCount * vectorsCount);
+            //float[] b = RandomHelper.GetNumbers(numbersOnVectorCount * vectorsCount);
+            //float[] resVect = new float[a.Length];
+            //float[] resArr = new float[a.Length];
+            //
+            //Stopwatch s = new Stopwatch();
+            //
+            //s.Restart();
+            //SumAsVectors(a, b, resVect, numbersOnVectorCount);
+            //s.Stop();
+            //Console.WriteLine(s.ElapsedMilliseconds);
+            //
+            //s.Restart();
+            //SumAsArrays(a, b, resArr);
+            //s.Stop();
+            //Console.WriteLine(s.ElapsedMilliseconds);
+        }
+
+        private static void SumAsVectors(float[] a, float[] b, float[] resVect, int numbersOnVectorCount)
+        {
+            //for (int i = 0; i < a.Length; i += numbersOnVectorCount)
+            //{
+            //    Vector<float> av = new Vector<float>(a, i);
+            //    Vector<float> bv = new Vector<float>(b, i);
+            //    Vector<float> rv = av + bv;
+            //    rv.CopyTo(resVect, i);
+            //    //(av + bv).CopyTo(resVect, i);
+            //    //Vector4 av = new Vector4(a[i], a[i+1], a[i+2], a[i+3]);
+            //    //Vector4 bv = new Vector4(b[i], b[i+1], b[i+2], b[i+3]);
+            //    //(av + bv).CopyTo(resVect, i);
+            //}
+        }
+
+        private static void SumAsArrays(float[] a, float[] b, float[] resArr)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                resArr[i] = a[i] + b[i];
+                //resArr[i + 1] = a[i + 1] + b[i + 1];
+                //resArr[i + 2] = a[i + 2] + b[i + 2];
+                //resArr[i + 3] = a[i + 3] + b[i + 3];
+            }
+        }
+    }
+}
