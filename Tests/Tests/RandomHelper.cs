209ae4f6abba9e8384d68695593bf09ed35362d4
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2017-02-19 13:10:29 +0300

    refactored simd tests

--- Tests/Tests/RandomHelper.cs
+++ Tests/Tests/RandomHelper.cs
@@ -6,11 +6,9 @@ namespace Tests
     {
         private static Random random = new Random(DateTime.Now.Millisecond);
 
-        public static float[] GetNumbers(int numbetCount)
+        public static float[] GetNumbers(int numbersCount)
         {
-            //if (Vector<float>.Count != 4)
-            //    throw new Exception("Current hardware suppor simd with count of float != 4. Reconsider test.");
-            float[] res = new float[numbetCount];
+            float[] res = new float[numbersCount];
             for (int i = 0; i < res.Length; i++)
             {
                 res[i] = random.Next() / (1.0f / int.MaxValue);
--- Tests/Tests/Tests/TestArrays.cs
+++ Tests/Tests/Tests/TestArrays.cs
@@ -4,44 +4,44 @@ namespace Tests.Tests
 {
 	public class TestArraysBoundsCheck
 	{
-        private int count;
-        float[] a;
-        float[] b; 
-        float[] res1;
-        float[] res2;
+		private int count;
+		float[] a;
+		float[] b;
+		float[] resultFixedCountRun;
+		float[] resultOptimizedRun;
 
-        public TestArraysBoundsCheck()
-        {
-            count = 100000000;
-            a = RandomHelper.GetNumbers(count);
-            b = RandomHelper.GetNumbers(count);
-            res1 = new float[count];
-            res2 = new float[count];
-        }
+		public TestArraysBoundsCheck()
+		{
+			count = 100000000;
+			a = RandomHelper.GetNumbers(count);
+			b = RandomHelper.GetNumbers(count);
+			resultFixedCountRun = new float[count];
+			resultOptimizedRun = new float[count];
+		}
 
-        [Benchmark]
-        public float[] Sum_slow()
-        {
-            for (int i = 0; i < count; i++)
-            {
-                res1[i] = a[i] + b[i];
-            }
-            return res1;
-        }
+		[Benchmark]
+		public float[] Sum_slow()
+		{
+			for (int i = 0; i < count; i++)
+			{
+				resultFixedCountRun[i] = a[i] + b[i];
+			}
+			return resultFixedCountRun;
+		}
 
-        [Benchmark]
-        public float[] Sum_fast()
-        {
-            // This caching is REALLY important.
-            float[] a_ = a;
-            float[] b_ = b;
-            float[] res2_ = res2;
+		[Benchmark]
+		public float[] Sum_fast()
+		{
+			// This caching is REALLY important.
+			float[] a_ = a;
+			float[] b_ = b;
+			float[] res2_ = resultOptimizedRun;
 
-            for (int i = 0; i < a_.Length; i++)
-            {
-                res2_[i] = a_[i] + b_[i];
-            }
-            return res2_;
-        }
-    }
+			for (int i = 0; i < a_.Length; i++)
+			{
+				res2_[i] = a_[i] + b_[i];
+			}
+			return res2_;
+		}
+	}
 }
