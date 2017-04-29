d39e4a8841841120f96bcaf58ab4a02e9a7d3e05
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2017-04-29 17:26:09 +0300

    added: test of lazy

---
+++ Tests/Tests/Tests/StructInitTest.cs
@@ -0,0 +1,70 @@
+﻿//  ----------------------------------------------------------------------------------------
+//   Module name: StructInitTest.cs
+//  
+//   Author:      Artyom Polishchuk
+//  
+//   Created at:  20-04-2017
+//  
+//   Description: 
+//  
+//  ----------------------------------------------------------------------------------------
+
+using System.CodeDom;
+using BenchmarkDotNet.Attributes;
+
+namespace Tests.Tests
+{
+	public class StructInitTest
+	{
+		public struct TestStruct
+		{
+			public float FloatVal;
+			public int IntVal;
+			public long LongVal;
+			public double DoubleVal;
+
+			public TestStruct(float floatVal, int intVal, long longVal, double doubleVal)
+			{ 
+				FloatVal = floatVal;		
+				IntVal = intVal;
+				LongVal = longVal;
+				DoubleVal = doubleVal;
+			}
+		}
+
+		private const int Count = 100000;
+		//private readonly TestStruct[] array = new TestStruct[Count];
+
+		private readonly int[] intArr = new int[Count];
+		private readonly float[] floatArr = new float[Count];
+		private readonly long[] longArr = new long[Count];
+		private readonly double[] doubleArr = new double[Count];
+
+
+		public StructInitTest()
+		{
+			floatArr = RandomHelper.GetFloatNumbers(Count);
+			intArr = RandomHelper.GetIntNumbers(Count);
+			longArr = RandomHelper.GetLongNumbers(Count);
+			doubleArr = RandomHelper.GetDoubleNumbers(Count);
+		}
+
+		[Benchmark]
+		public TestStruct GetWithConstr()
+		{
+			return new TestStruct(1f, 1, 1L, 1.0);
+		}
+
+		[Benchmark]
+		public TestStruct GetWithInitializer()
+		{
+			return new TestStruct()
+			{
+				IntVal = 1,
+				DoubleVal = 1.0,
+				FloatVal = 1.0f,
+				LongVal = 1L,
+			};
+		}
+	}
+}
