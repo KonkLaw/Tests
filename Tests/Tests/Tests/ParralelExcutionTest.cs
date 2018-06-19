ad9c5c07775c158f6c3a180446cdd9bc064c349d
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2018-06-19 10:09:14 +0300

    added: parallel computation test

--- Tests/Tests/Tests/ParralelExcutionTest.cs
+++ Tests/Tests/Tests/ParralelExcutionTest.cs
@@ -3,13 +3,31 @@ using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Tests.Tests.BaseDataTypes;
 
+//	BenchmarkDotNet=v0.10.7, OS=Windows 10.0.17134
+//	Processor=Intel Core i7-8700K CPU 3.70GHz, ProcessorCount=12
+//	Frequency=3609382 Hz, Resolution=277.0557 ns, Timer=TSC
+//[Host]     : Clr 4.0.30319.42000, 64bit RyuJIT-v4.7.3110.0
+//DefaultJob : Clr 4.0.30319.42000, 64bit RyuJIT-v4.7.3110.0
+
+
+//Method |       Mean |      Error |     StdDev |
+//--------------------------- |-----------:|-----------:|-----------:|
+//SingleTread | 3,994.9 us | 58.3344 us | 54.5660 us |
+//Parralel12OptimizedByCalls |   626.4 us |  4.5140 us |  4.0015 us |
+//ParralelDefault |   673.2 us |  1.0565 us |  0.8822 us |
+//Parralel12 |   678.2 us |  1.0149 us |  0.7924 us |
+//Parralel6 |   943.6 us | 17.4942 us | 16.3641 us |
+//Parralel4 |   940.3 us | 18.5411 us | 19.0403 us |
+//Parralel14 |   683.7 us |  1.0010 us |  0.7815 us |
+//Parralel16 |   685.8 us |  0.8335 us |  0.7796 us |
+
 namespace Tests.Tests
 {
 	public class ParralelExcutionTest
 	{
 		private readonly Matrix4F[] inPrams;
 		private readonly Matrix4F[] outPrams;
-		private const int MatricesCount = 100_000;
+		private const int MatricesCount = 120_000;
 
 		public ParralelExcutionTest()
 		{
@@ -26,6 +44,30 @@ namespace Tests.Tests
 			for (int i = 0; i < inPrams.Length; i++)
 			{
 				Matrix4F.Invert(_in[i], out _out[i]);
+			}
+		}
+
+		private int countInBatch;
+		[Benchmark]
+		public void Parralel12OptimizedByCalls()
+		{
+			int batches = Environment.ProcessorCount;
+			countInBatch = MatricesCount / batches;
+			Parallel.For(0, batches, new ParallelOptions()
+			{
+				MaxDegreeOfParallelism = batches,
+			}, InvertStartingFrom);
+		}
+		private void InvertStartingFrom(int batchIndex)
+		{
+			Matrix4F[] _in = inPrams;
+			Matrix4F[] _res = outPrams;
+			int startIndex = batchIndex * countInBatch;
+			int stopIndex = Math.Min(startIndex + countInBatch, _in.Length);
+
+			for (int i = startIndex; i < stopIndex; i++)
+			{
+				Matrix4F.Invert(_in[i], out _res[i]);
 			}
 		}
 
