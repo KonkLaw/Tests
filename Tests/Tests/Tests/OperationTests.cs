ba498b98cad775af36c2dc714e66dbf62d19f640
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2018-09-10 13:04:50 +0300

    added: operation tests

---
+++ Tests/Tests/Tests/OperationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Tests.Tests
+{
+	public class OperationTests
+	{
+		private float a;
+		private float b;
+
+		public OperationTests()
+		{
+			a = RandomHelper.GetFloat();
+			b = RandomHelper.GetFloat();
+		}
+
+		[Benchmark]
+		public double LogTest()
+		{
+			return Math.Log10(a);
+		}
+
+		[Benchmark]
+		public double SqrtTest()
+		{
+			return Math.Sqrt(a);
+		}
+
+		[Benchmark]
+		public float SumTest()
+		{
+			return a + b;
+		}
+
+	}
+}
