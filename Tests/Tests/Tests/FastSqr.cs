d0997c850c80b826a3f76202b1d3e13e37277064
Artyom Polishchuk <polishchuk.artyom@gmail.com>
2019-01-10 11:28:44 +0300

    added: sqrt test

---
+++ Tests/Tests/Tests/FastSqr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Tests.Tests
+{
+	public class FastSqr
+	{
+		private double random;
+
+		public FastSqr()
+		{
+			random = RandomHelper.GetDoubleNumbers(1)[0];
+		}
+
+		[Benchmark]
+		public double Multipl() => random * random;
+
+		[Benchmark]
+		public double MathPow() => Math.Pow(random, 2);
+	}
+}
