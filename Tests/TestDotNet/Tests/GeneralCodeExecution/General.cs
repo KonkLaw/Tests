76d5b1243b5edcca806cdb20e50676afc0133866
Konklaw <22473930+KonkLaw@users.noreply.github.com>
2022-04-15 11:23:39 +0300

    added: expression tests

--- Tests/TestDotNet/Tests/GeneralCodeExecution/General.cs
+++ Tests/TestDotNet/Tests/GeneralCodeExecution/General.cs
@@ -3,20 +3,19 @@ using System.Linq.Expressions;
 
 namespace TestDotNet.Tests.GeneralCodeExecution;
 
-
-//BenchmarkDotNet=v0.13.1, OS=Windows 10.0.19043.1586 (21H1/May2021Update)
+//BenchmarkDotNet=v0.13.1, OS=Windows 10.0.19043.1645 (21H1/May2021Update)
 //Intel Core i7-8700K CPU 3.70GHz(Coffee Lake), 1 CPU, 12 logical and 6 physical cores
-//.NET SDK= 6.0.201
-// [Host]     : .NET 6.0.3 (6.0.322.12309), X64 RyuJIT
-//  DefaultJob : .NET 6.0.3 (6.0.322.12309), X64 RyuJIT
+//.NET SDK= 6.0.202
 //|           Method |           Mean |       Error |      StdDev |
 //|----------------- |---------------:|------------:|------------:|
-//| CreateExpression |    517.5797 ns |   1.5716 ns |   1.4700 ns |
-//|    GetExpression |      1.5940 ns |   0.0103 ns |   0.0097 ns |
-//|          GetName |      1.9134 ns |   0.0063 ns |   0.0059 ns |
-//|    CompileCached | 39,562.3162 ns | 430.4176 ns | 359.4182 ns |
-//|   CompileCreated | 42,035.0004 ns | 410.3830 ns | 383.8725 ns |
-//|         CastTest |      0.1616 ns |   0.0037 ns |   0.0029 ns |
+//|  CreateFunction1 |      4.2774 ns |   0.1024 ns |   0.0958 ns |
+//|   CreateFunctio2 |      0.7251 ns |   0.0290 ns |   0.0257 ns |
+//| CreateExpression |    546.4990 ns |   2.9436 ns |   2.7535 ns |
+//|    GetExpression |      1.5163 ns |   0.0065 ns |   0.0058 ns |
+//|          GetName |      1.9016 ns |   0.0589 ns |   0.0492 ns |
+//|    CompileCached | 41,277.5775 ns | 778.8589 ns | 728.5451 ns |
+//|   CompileCreated | 43,656.7480 ns | 589.3296 ns | 551.2592 ns |
+//|         CastTest |      0.4258 ns |   0.0509 ns |   0.0476 ns |
 
 public class General
 {
@@ -25,15 +24,24 @@ public class General
 
     private object GetNumbers() => numbers;
 
-    private Expression<Func<List<int>, int>> CraeteExpressionLambda()
-    {
-        return list => list.Count;
-    }
+    private int TestFunc(List<int> list) => list.Count;
+
+    private Func<List<int>, int> CreateFunction_1() => TestFunc;
+
+    private Func<List<int>, int> CreateFunction_2() => list => list.Count;
+
+    [Benchmark]
+    public Func<List<int>, int> CreateFunction1() => CreateFunction_1();
+
+    [Benchmark]
+    public Func<List<int>, int> CreateFunctio2() => CreateFunction_2();
+
+    private Expression<Func<List<int>, int>> CraeteExpressionLambda_() => list => list.Count;
 
     [Benchmark]
     public Expression<Func<List<int>, int>> CreateExpression()
     {
-        return CraeteExpressionLambda();
+        return CraeteExpressionLambda_();
     }
 
     [Benchmark]
@@ -57,7 +65,7 @@ public class General
     [Benchmark]
     public Func<List<int>, int> CompileCreated()
     {
-        return CraeteExpressionLambda().Compile();
+        return CraeteExpressionLambda_().Compile();
     }
 
     [Benchmark]
